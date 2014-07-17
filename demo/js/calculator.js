631a58413c50406e40fd89202eb242830f133e63
João Lucas <jlucasps@gmail.com>
2014-07-17 00:31:26 -0300

    Terminologia spies, stubs, mocks e fakes

--- demo/js/calculator.js
+++ demo/js/calculator.js
@@ -1,11 +1,17 @@
 var Calculator = function() {
 
-  var api = this;
+    var api = this;
 
-  api.add = function(a, b) {
-    if (isNaN(a) || isNaN(b)) throw 'Only numbers are valid';
-    return a + b;
-  }
+    api.add = function(a, b) {
+        if (isNaN(a) || isNaN(b)) throw 'Only numbers are valid';
+        return a + b;
+    }
 
-  return api;
+    api.div = function(a, b) {
+        if (isNaN(a) || isNaN(b)) throw 'Only numbers are valid';
+        if ( b === 0) throw 'Can not divide by zero';
+        return ( a / b ) * 1.00;
+    }
+
+    return api;
 };
